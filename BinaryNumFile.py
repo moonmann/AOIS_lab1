542cdb7a5a4840c465517380c162e9bde9b81a70
Андрей <88152811+moonmann@users.noreply.github.com>
2023-03-11 13:51:36 +0300

    fix

--- BinaryNumFile.py
+++ BinaryNumFile.py
@@ -1,28 +1,35 @@
+NUM_BITS = 16
+BIN_ONE = '1'
+BIN_ONE = BIN_ONE.rjust(NUM_BITS, '0')
+BIN_ZERO = '0'
+BIN_ZERO = BIN_ZERO.rjust(NUM_BITS, '0')
+
+
 def binary_add_helper(number_1, number_2):
     result = ''
     carry = 0
-    for i in range(15, -1, -1):
-        r = carry
-        r += 1 if number_1[i] == '1' else 0
-        r += 1 if number_2[i] == '1' else 0
-        result = ('1' if r % 2 == 1 else '0') + result
-        carry = 0 if r < 2 else 1
+    for i in range(NUM_BITS - 1, -1, -1):
+        bit_sum = carry
+        bit_sum += 1 if number_1[i] == '1' else 0
+        bit_sum += 1 if number_2[i] == '1' else 0
+        result = ('1' if bit_sum % 2 == 1 else '0') + result
+        carry = 0 if bit_sum < 2 else 1
     return result
 
 
 def binary_sub_helper(number_1, number_2):
     result = ''
-    temp = 0
-    for i in range(15, - 1, - 1):
-        num = int(number_1[i]) - int(number_2[i]) - temp
+    carry = 0
+    for i in range(NUM_BITS - 1, - 1, - 1):
+        num = int(number_1[i]) - int(number_2[i]) - carry
         if num % 2 == 1:
             result = '1' + result
         else:
             result = '0' + result
         if num < 0:
-            temp = 1
+            carry = 1
         else:
-            temp = 0
+            carry = 0
     if int(result) == 0:
         result = 0
     return result
@@ -46,7 +53,7 @@ class BinaryNum:
         sign = 0
         number = self.dec
         if number == 0:
-            answer = answer.rjust(16, '0')
+            answer = BIN_ZERO
             return answer
         if number < 0:
             sign = 1
@@ -55,16 +62,16 @@ class BinaryNum:
             answer += str(number % 2)
             number = int(number / 2)
         answer = answer[::-1]
-        answer = answer.rjust(16, '0')
+        answer = answer.rjust(NUM_BITS, '0')
         answer = str(sign) + answer[1:]
         return answer
 
     def binary_to_decimal(self):
-        temp = self.bin_right
-        sign = int(temp[0])
+        right_code = self.bin_right
+        sign = int(right_code[0])
         if sign:
-            temp = '0' + temp[1:]
-        binary = int(temp)
+            right_code = '0' + right_code[1:]
+        binary = int(right_code)
         decimal, i = 0, 0
         while binary != 0:
             dec = binary % 10
@@ -94,12 +101,12 @@ class BinaryNum:
         if self.dec >= 0:
             return self.bin_right
         answer = self.bin_reverse
-        answer = binary_add_helper(answer, '0000000000000001')
+        answer = binary_add_helper(answer, BIN_ONE)
         return answer
 
     def from_additional_to_reverse(self):
         if int(self.bin_additional[0]):
-            answer = binary_sub_helper(self.bin_additional, '0000000000000001')
+            answer = binary_sub_helper(self.bin_additional, BIN_ONE)
             return answer
         else:
             return self.bin_additional
@@ -142,23 +149,23 @@ def binary_sub(number_1, number_2):
 
 def binary_div_helper(number_1, number_2):
     result = ''
-    temp = '0'
-    r = 0
+    temp_decreased = '0'
+    difference = 0
     for i in range(len(number_1)):
-        if int(number_2) > int(temp):
+        if int(number_2) > int(temp_decreased):
             result += '0'
-            temp += number_1[i]
+            temp_decreased += number_1[i]
         else:
-            temp = temp.rjust(16, '0')
-            r = binary_sub_helper(temp, number_2)
-            if r == 0:
-                temp = number_1[i]
+            temp_decreased = temp_decreased.rjust(NUM_BITS, '0')
+            difference = binary_sub_helper(temp_decreased, number_2)
+            if difference == 0:
+                temp_decreased = number_1[i]
                 result += '1'
             else:
-                r = str(r).lstrip('0')
+                difference = str(difference).lstrip('0')
                 result += '1'
-                temp = r + number_1[i]
-    if int(temp) != 0:
+                temp_decreased = difference + number_1[i]
+    if int(temp_decreased) != 0:
         result = result + '1'
     else:
         result = result + '0'
@@ -200,8 +207,8 @@ def binary_div(number_1, number_2):
         return BinaryNum(0, binary_div_helper(BinaryNum(number_1.dec * -1).bin_right,
                                               BinaryNum(number_2.dec * -1).bin_right))
     elif number_1.dec > 0 > number_2.dec:
-        temp = BinaryNum(0, binary_div_helper(number_1.bin_right, BinaryNum(number_2.dec * -1).bin_right))
-        return binary_mult(temp, BinaryNum(-1))
+        quotient = BinaryNum(0, binary_div_helper(number_1.bin_right, BinaryNum(number_2.dec * -1).bin_right))
+        return binary_mult(quotient, BinaryNum(-1))
     elif number_2.dec > 0 > number_1.dec:
-        temp = BinaryNum(0, binary_div_helper(BinaryNum(number_1.dec * -1).bin_right, number_2.bin_right))
-        return binary_mult(temp, BinaryNum(-1))
+        quotient = BinaryNum(0, binary_div_helper(BinaryNum(number_1.dec * -1).bin_right, number_2.bin_right))
+        return binary_mult(quotient, BinaryNum(-1))
--- main.py
+++ main.py
@@ -4,19 +4,19 @@ import BinaryNumFile
 
 def main():
     while True:
-        num1 = BinaryNumFile.BinaryNum(int(input("Enter the first int number: ")))
+        """num1 = BinaryNumFile.BinaryNum(int(input("Enter the first int number: ")))
         num1.print_num()
         num2 = BinaryNumFile.BinaryNum(int(input("Enter the second int number: ")))
-        num2.print_num()
+        num2.print_num()"""
 
-        print('Binary sum: ')
+        """print('Binary sum: ')
         BinaryNumFile.binary_add(num1, num2).print_num()
         print('Binary sub: ')
         BinaryNumFile.binary_sub(num1, num2).print_num()
         print('Binary mult: ')
         BinaryNumFile.binary_mult(num1, num2).print_num()
         print('Binary div: ')
-        BinaryNumFile.binary_div(num1, num2).print_num()
+        BinaryNumFile.binary_div(num1, num2).print_num()"""
 
         num3 = float(input("Enter the first float number: "))
         num4 = float(input("Enter the second float number: "))
